cf08ece5687802758922dcbea5f36fcf0dbf3d45
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-07-22 14:29:19 -0700

    Erroe thrown

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import * as firebase from "firebase";
-import RoomList from "./components/RoomList";
+import RoomList from "./components/RoomList.js";
 
 var config = {
   apiKey: "AIzaSyDQ1h7AQ6d7rXV3sRQS5K4l7YhG0cyIYKw",
@@ -18,13 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RoomList firebase={firebase} />
       </div>
     );
   }
