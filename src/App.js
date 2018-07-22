205ec9f0a0395dbe7878a2231ae5ad32b174e6e8
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-07-22 09:50:18 -0700

    Started Firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as firebase from "firebase";
+import RoomList from "./components/RoomList";
+
+var config = {
+  apiKey: "AIzaSyDQ1h7AQ6d7rXV3sRQS5K4l7YhG0cyIYKw",
+  authDomain: "bloc-chat-react-a30fd.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-a30fd.firebaseio.com",
+  projectId: "bloc-chat-react-a30fd",
+  storageBucket: "bloc-chat-react-a30fd.appspot.com",
+  messagingSenderId: "251674011163"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
